4589cf638cb76c0415ad3512375e808ef94e747a
Syx Pek <20557013+SyxP@users.noreply.github.com>
2017-08-01 17:00:23 +0800

    Update TelegramGame.java

--- TelegramBot/TelegramGame.java
+++ TelegramBot/TelegramGame.java
@@ -3,13 +3,22 @@ package TelegramBot;
 import Core.*
 
 public class TelegramGame extends Game{
-    private long chat_ID;
+    private long chat_ID,msg_ID;
     public TelegramGame(long ID){
         super();
         chat_ID = ID;
+        msg_ID = -1L;
     }
 
     public long getChatID(){
         return chat_ID;
+    }
+
+    public long getMsgID(){
+        return msg_ID;
+    }
+
+    public void setMsgID(long newID){
+        msg_ID = newID;
     }
 }
