760f6eccfb5dbc91bb6278d643c2367f8d488aa3
Syx Pek <20557013+SyxP@users.noreply.github.com>
2017-08-01 14:08:52 +0800

    Create TelegramPlayer.java

---
+++ TelegramBot/TelegramPlayer.java
@@ -0,0 +1,14 @@
+package Telegram;
+
+public class TelegramPlayer extends Player{
+    private long chat_ID;
+
+    public TelegramPlayer(String s, long ID){
+        super(s);
+        chat_ID = ID;
+    }
+
+    public long getChatID(){
+        return chat_ID;
+    }
+}
